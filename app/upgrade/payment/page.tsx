2f1ba8bc1e17c53305f3e934bd10142fbb2a44de
donb01326-hue <donb01326@gmail.com>
2026-08-16 15:32:51 +0100

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("6734393611")
+    navigator.clipboard.writeText("3000843737")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -95,7 +95,7 @@ export default function UpgradePaymentPage() {
                 <span>🔢</span> Account Number
               </p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">6734393611</p>
+                <p className="font-bold">3000843737</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -116,7 +116,7 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">Paygo-AKINYEMI EMMANUEL</p>
+              <p className="font-bold">Paygo-Rotimi Clement Akinyemi</p>
             </div>
           </div>
 
